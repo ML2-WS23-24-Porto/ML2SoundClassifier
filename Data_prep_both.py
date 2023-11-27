635a87a14855e7d34dc21b15e26dc8b0e33ba526
EliasDSAI <elias.88348@gmail.com>
2023-11-27 03:54:42 +0000

    added some stuff

--- Data_prep_both.py
+++ Data_prep_both.py
@@ -9,7 +9,7 @@ import seaborn as sns
 import tqdm
 import os
 
-# for downloading the dataset
+# for downloading the dataset, put the sound dataset in the same folder as the file after downloading
 #dataset = soundata.initialize('urbansound8k')
 #dataset.download()  # download the dataset
 #dataset.validate()  # validate that all the expected files are there
@@ -17,44 +17,37 @@ import os
 #example_clip = dataset.choice_clip()  # choose a random example clip
 #print(example_clip)
 
-dataset = soundata.initialize(dataset_name='urbansound8k',data_home="/home/elias88348/sound_datasets/urbansound8k")
-ids = dataset.clip_ids  # the list of urbansound8k's clip ids
-clips = dataset.load_clips()  # Load all clips in the dataset
-example_clip = clips[ids[0]]  # Get the first clip
-clip_info = example_clip.slice_file_name
-y, sr = example_clip.audio
-print(clip_info)
-
-metadata = pd.read_csv('/home/elias88348/sound_datasets/urbansound8k/metadata/UrbanSound8K.csv')
-metadata.head(10)
-#print(metadata)
-
-def data_cleaning(): #here we should perform noise reduction , zero padding and resampling,...?
-    target_sr = 16000
-    y_downsampled = librosa.resample(y, sr, target_sr)
-    return y_downsampled
 
 
-def get_mfcc(): # this function gets the mfcc
-    (sig, rate) = librosa.load(f, sr=None)
-    sig_res = librosa.resample(sig, orig_sr=rate, target_sr=target_sr)
+def data_cleaning(y,sr,target_sr = 16000): #here we should perform noise reduction , zero padding and resampling,...?
+    y_resampled = librosa.resample(y, orig_sr=sr, target_sr=target_sr)
     # zero padding
-    if len(sig_res) < 4 * target_sr:
-        sig_res_pad = np.concatenate((sig_res, np.zeros(4 * target_sr - len(sig_res))), axis=0)
+    if len(y_resampled) < 4 * target_sr:
+        sig = np.concatenate((y_resampled, np.zeros(4 * target_sr - len(y_resampled))), axis=0)
     else:
-        sig_res_pad = sig_res
-    dataset[i] = sig_res_pad
-    # computes the MFCCs
-    sig_mfcc = librosa.feature.mfcc(y=sig_res_pad, sr=target_sr, n_fft=n_fft, hop_length=hop_length,
-                                    win_length=win_length, n_mfcc=n_mfcc)
-    return
+        sig = y_resampled
+    return sig
 
-def get_mel_spec(): # this function gets the mel spectogram
-    S = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=128, fmax=8000)
-    fig, ax = plt.subplots()
+
+
+def get_mfcc(y,sr): # this function gets the mfcc
+    # MFCC parameters
+    n_mfcc = 40
+    hop_length = round(target_sr * 0.0125)
+    win_length = round(target_sr * 0.023)
+    n_fft = 2 ** 14
+    mfcc_time_size = 4 * target_sr // hop_length + 1
+    # computes the MFCCs
+    S = librosa.feature.mfcc(y=y, sr=sr, n_fft=n_fft, hop_length=hop_length,
+                                    win_length=win_length, n_mfcc=n_mfcc)
+    return S
+
+
+def get_mel_spec(y,sr): # this function gets the mel spectogram
+    n_fft = 2 ** 14
+    S = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=128, fmax=8000,n_fft=n_fft)
     S_dB = librosa.power_to_db(S, ref=np.max)
-    df = pd.DataFrame(S_dB)
-    return
+    return S_dB
 
 def normalize_spectogram(clip): # bring spectograms in the form we want, suitable for ml models
     # Create a MinMaxScaler
@@ -67,6 +60,52 @@ def normalize_spectogram(clip): # bring spectograms in the form we want, suitabl
     print("\nNormalized DataFrame:")
     print(df_normalized)
 
+
+def visualize(S,sr):
+    fig, ax = plt.subplots()
+    img = librosa.display.specshow(S, x_axis='time', y_axis='mel', sr=sr, fmax=8000, ax=ax)
+    fig.colorbar(img, ax=ax, format='%+2.0f dB')
+    ax.set(title=f'Mel-frequency spectrogram of clip {clip_info} with samplerate {sr} Hz')
+    plt.show()
+
+
+# main loop for data prep:
+if __name__== "__main__":
+    # Metadata
+    metadata = pd.read_csv('sound_datasets/urbansound8k/metadata/UrbanSound8K.csv')
+    metadata.head(10)
+    # print(metadata)
+    dataset = soundata.initialize(dataset_name='urbansound8k', data_home=r"sound_datasets/urbansound8k")
+    ids = dataset.clip_ids  # the list of urbansound8k's clip ids
+    clips = dataset.load_clips()  # Load all clips in the dataset
+    example_clip = clips[ids[0]]  # Get the first clip
+    clip_info = example_clip.slice_file_name
+    y, sr = example_clip.audio
+    print(clip_info)
+
+
+
+
+
+    # example processing
+    target_sr = 16000
+    sig = data_cleaning(y,sr,target_sr=target_sr)
+    sig_mfcc = get_mfcc(sig,target_sr)
+    sig_melspec = get_mel_spec(sig,target_sr)
+
+
+    visualize(sig_mfcc,target_sr)
+    visualize(sig_melspec,target_sr)
+
+
+
+
+    #for index_num, row in tqdm.tqdm(df.iterrows(), total=len(df)):
+    #    file = os.path.join(
+    #        os.path.abspath('/kaggle/input/urbansound8k/'), "fold" + str(row["fold"]) + "/",
+    #        str(row["slice_file_name"]),
+    #    )
+    #    label = row["class"]
+
+
 # after this ml model
-
-
